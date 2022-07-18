31aca6a7161601afec2b655e19f258547f6d0467
shuhanmirza-bk <mirza.shuhan@bkash.com>
2022-07-18 13:14:45 +0600

    add binding

--- go-mw/go-mw-service/list_event_api.go
+++ go-mw/go-mw-service/list_event_api.go
@@ -8,7 +8,7 @@ import (
 )
 
 type GetRandomRequest struct {
-	Length int `json:"length" binding:"gt=0"`
+	Length int `json:"length" binding:"gt=0,lt=999999"`
 }
 
 type GetRandomResponse struct {
@@ -27,6 +27,11 @@ func (server *Server) getRandom(ctx *gin.Context) {
 
 		fmt.Printf("%v", response)
 		ctx.JSON(http.StatusOK, response)
+		return
+	} else {
+		fmt.Println("validation error")
+		fmt.Println(err)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 }
